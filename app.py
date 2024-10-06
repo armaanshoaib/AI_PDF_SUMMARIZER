9e43554c9ec18de61c9940d1db829a561d18a6ac
armaanshoaib <76841030+armaanshoaib@users.noreply.github.com>
2024-10-06 20:29:31 +0530

    added JSP

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def extract_text(file_path):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port='5000',debug=True)
